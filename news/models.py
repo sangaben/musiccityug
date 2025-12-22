7b99f3117ee5c91595997cac029fc7cad2a93078
Sanga Ben <122868410+sangaben@users.noreply.github.com>
2025-12-23 00:39:51 +0300

    Update models.py

--- news/models.py
+++ news/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
+
 
 class NewsArticle(models.Model):
     CATEGORY_CHOICES = [
